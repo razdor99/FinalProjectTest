d4be61be7d6180e09ebff594f4236badd661ac38
Nicholas Vece <91498866+razdor99@users.noreply.github.com>
2021-12-01 00:22:55 +0000

    d

--- lcd_i2c.py
+++ lcd_i2c.py
@@ -4,8 +4,8 @@ import time
 
 def setup():
 	LCD1602.init(0x27, 1)	# init(slave address, background light)
-	LCD1602.write(5, 0, '8:30 am')
-	LCD1602.write(0, 1, 'Good Morning Nick')
+	LCD1602.write(5, 0, 'shane is a')
+	LCD1602.write(0, 1, 'wayback bitch')
 	time.sleep(2)
 
 def destroy():
