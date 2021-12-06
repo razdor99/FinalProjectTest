3530c3193b94b48bb1a368fdfd3997e5b14582a6
Nicholas Vece <91498866+razdor99@users.noreply.github.com>
2021-12-06 20:39:07 +0000

    e

--- hardwareTest.py
+++ hardwareTest.py
@@ -2,5 +2,5 @@ import LCD1602
 
 while True:
   LCD1602.init(0x27, 1)
-  LCD1602.write(4, 0, 'Hello')
-  LCD1602.write(7, 1, 'REXQUALIS!')
+  LCD1602.write(0, 0, 'Hello')
+  LCD1602.write(0, 1, 'REXQUALIS!')
