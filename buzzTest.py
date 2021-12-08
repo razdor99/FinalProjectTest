a261dd5fcac05c2c669e5782e03abc3d88fd4c80
Nicholas Vece <91498866+razdor99@users.noreply.github.com>
2021-12-08 19:31:22 +0000

    s

--- buzzTest.py
+++ buzzTest.py
@@ -9,10 +9,7 @@ CL = [0, 131, 147, 165, 175, 196, 211, 248]		# Frequency of Low C notes
 CM = [0, 262, 294, 330, 350, 393, 441, 495]		# Frequency of Middle C notes
 
 CH = [0, 525, 589, 661, 700, 786, 882, 990]		# Frequency of High C notes
-song_1 = [	CM[3], CM[5], CM[6], CM[3], CM[2], CM[3], CM[5], CM[6], # Notes of song1
-			CH[1], CM[6], CM[5], CM[1], CM[3], CM[2], CM[2], CM[3],
-			CM[5], CM[2], CM[3], CM[3], CL[6], CL[6], CL[6], CM[1],
-			CM[2], CM[3], CM[2], CL[7], CL[6], CM[1], CL[5],CM[3], CM[2], CL[7], CL[6], CM[1], CL[5]	,CM[3], CM[2], CL[7], CL[6], CM[1], CL[5]	,CM[3], CM[2], CL[7], CL[6], CM[1], CL[5]	,CM[3], CM[2], CL[7], CL[6], CM[1], CL[5]	,CM[3], CM[2], CL[7], CL[6], CM[1], CL[5]	,CM[3], CM[2], CL[7], CL[6], CM[1], CL[5]	,CM[3], CM[2], CL[7], CL[6], CM[1], CL[5]	]
+song_1 = [	CM[3], CM[5], CM[6], CM[3], CM[2], CM[3], CM[5], CM[6], ]
       
 def buzzsetup():
 	GPIO.setmode(GPIO.BOARD)		# Numbers GPIOs by physical location
